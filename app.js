8de8c318d3f4dbe3c8c70b37a319dd5fc5580ba7
Lox-01 <128305675+Lox-01@users.noreply.github.com>
2025-02-21 07:18:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('data.json')
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            for (const (name, message) of data) {
+                console.log(name);
+                console.log(message);
+            }
         })
         .catch(error => {
             document.getElementById("jsonContent").innerText = "Failed to load data.";
