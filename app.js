b92569bfaf3ad378b5e628f4f974137a2ae34cdf
Lox-01 <128305675+Lox-01@users.noreply.github.com>
2025-02-21 09:02:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.json())
         .then(data => {
             for (const [name, message] of Object.entries(data)) {
-                document.querySelector("div.box").innerHTML += `<div class="repoEntry"><div class="repoName">${name}</div><div class="repoMessage">${message}</div></div>`
+                document.querySelector("div.box").innerHTML += `<div class="repoEntry"><div class="repoName">${name}</div><div class="repoMessage">${message[0]}</div></div>`
             }
         })
         .catch(error => {
