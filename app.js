d2aa5ed7e4181507d865a4bcf3fe58e35eee8d5b
Lox-01 <128305675+Lox-01@users.noreply.github.com>
2025-02-21 07:20:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('data.json')
         .then(response => response.json())
         .then(data => {
-            for (const (name, message) of data) {
+            for (const [name, message] of data) {
                 console.log(name);
                 console.log(message);
             }
