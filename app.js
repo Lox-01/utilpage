da7fd07cc5289a5fdab4c1c0edcb553f88e75cbe
Lox-01 <128305675+Lox-01@users.noreply.github.com>
2025-02-21 09:05:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+function nbsp(num) {
+    return '&nbsp;'.repeat(num)
+}
 document.addEventListener("DOMContentLoaded", function() {
     fetch('data.json')
         .then(response => response.json())
         .then(data => {
             for (const [name, message] of Object.entries(data)) {
-                document.querySelector("div.box").innerHTML += `<div class="repoEntry"><div class="repoName">${name}</div><div class="repoMessage">${message[0]}</div></div>`
+                document.querySelector("div.box").innerHTML += `<div class="repoEntry"><div class="repoName">${name}${nbsp(message[2])}</div><div class="repoMessage">${message[0]}</div></div>`
             }
         })
         .catch(error => {
